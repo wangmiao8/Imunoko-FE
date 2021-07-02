f6d6fd5c03c7483ef684f7c6f7cce689465559b8
noko <lianggang129@vip.qq.com>
2021-07-02 23:38:11 +0800

    code3

--- part1/fed-e-task-01-01/code/support.js
+++ part1/fed-e-task-01-01/code/support.js
@@ -9,3 +9,20 @@ class Container {
     return Container.of(fn(this._value));
   }
 }
+
+class Maybe {
+  static of(x) {
+    return new Maybe(x);
+  }
+  constructor(x) {
+    this._value = x;
+  }
+  isNothing() {
+    return this._value === null || this._value === undefined;
+  }
+  map(fn) {
+    return this.isNothing() ? this : new Maybe(fn(this._value));
+  }
+}
+
+module.exports = { Container, Maybe };
