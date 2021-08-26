8aee0bc5f95681844e87e724a6fca78a909cf08f
noko <lianggang@szhcf.com.cn>
2021-08-27 01:38:55 +0800

    add gulpfile

--- part2/fed-e-task-02-01/code/pages-boilerplate/gulpfile.js
+++ part2/fed-e-task-02-01/code/pages-boilerplate/gulpfile.js
@@ -1,1 +1,30 @@
 // 实现这个项目的构建任务
+
+const { src, dest } = require("gulp");
+const swig = require("gulp-swig");
+const sass = require("gulp-sass")(require("sass"));
+const babel = require("gulp-babel");
+
+const page = () => {
+  return src("src/*.html", { base: "src" })
+    .pipe(swig())
+    .pipe(dest("dist"));
+};
+
+const style = () => {
+  return src("src/assets/styles/*.scss", { base: "src" }) // base 保留文件路径
+    .pipe(sass())
+    .pipe(dest("dist"));
+};
+
+const script = () => {
+  return src("src/assets/scripts/*.js", { base: "src" }) // base 保留文件路径
+    .pipe(babel({ presets: ["@babel/preset-env"] }))
+    .pipe(dest("dist"));
+};
+
+module.exports = {
+  style,
+  script,
+  page
+};
