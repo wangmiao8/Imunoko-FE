aff98852b407ecc00610b87e6f9fc02ff951684b
noko <lianggang@szhcf.com.cn>
2021-09-07 02:12:59 +0800

    update serve task

--- part2/fed-e-task-02-01/code/pages-boilerplate/gulpfile.js
+++ part2/fed-e-task-02-01/code/pages-boilerplate/gulpfile.js
@@ -1,18 +1,69 @@
 // 实现这个项目的构建任务
 
-const { src, dest, series, parallel } = require("gulp");
+const { src, dest, series, parallel, watch } = require("gulp");
 
 const del = require("del");
+const bs = require("browser-sync").create();
 
 // 自动 require 所有的 gulp 插件
-const plugins = require('gulp-load-plugins')();
-const sass = plugins.sass(require('sass')); // sass 的引入方法需要特殊处理下
+const plugins = require("gulp-load-plugins")();
+const sass = plugins.sass(require("sass")); // sass 的引入方法需要特殊处理下
+
+// 模板数据
+const data = {
+  menus: [
+    {
+      name: "Home",
+      icon: "aperture",
+      link: "index.html",
+    },
+    {
+      name: "Features",
+      link: "features.html",
+    },
+    {
+      name: "About",
+      link: "about.html",
+    },
+    {
+      name: "Contact",
+      link: "#",
+      children: [
+        {
+          name: "Twitter",
+          link: "https://twitter.com/w_zce",
+        },
+        {
+          name: "About",
+          link: "https://weibo.com/zceme",
+        },
+        {
+          name: "divider",
+        },
+        {
+          name: "About",
+          link: "https://github.com/zce",
+        },
+      ],
+    },
+  ],
+  pkg: require("./package.json"),
+  date: new Date(),
+};
 
 /**
  * @desc: HTML 编译
  */
 const page = () => {
-  return src("src/*.html", { base: "src" }).pipe(plugins.swig()).pipe(dest("dist"));
+  return src("src/*.html", { base: "src" })
+    .pipe(
+      plugins.swig({
+        data,
+        defaults: { cache: false },
+      })
+    )
+    .pipe(dest("dist"))
+    .pipe(bs.reload({ stream: true }));
 };
 
 /**
@@ -20,8 +71,9 @@ const page = () => {
  */
 const style = () => {
   return src("src/assets/styles/*.scss", { base: "src" }) // base 保留文件路径
-    .pipe(sass())
-    .pipe(dest("dist"));
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(dest("dist"))
+    .pipe(bs.reload({ stream: true }));
 };
 
 /**
@@ -30,7 +82,8 @@ const style = () => {
 const script = () => {
   return src("src/assets/scripts/*.js", { base: "src" }) // base 保留文件路径
     .pipe(plugins.babel({ presets: ["@babel/preset-env"] }))
-    .pipe(dest("dist"));
+    .pipe(dest("dist"))
+    .pipe(bs.reload({ stream: true }));
 };
 
 /**
@@ -63,6 +116,34 @@ const extra = () => {
  */
 const clean = () => del(["dist"]);
 
+/**
+ * @desc: 启动服务，HRM
+ */
+const serve = () => {
+  // 监视文件改动，执行指定任务
+  watch("./src/*.html", page);
+  watch("./src/assets/styles/*.scss", style);
+  watch("./src/assets/scripts/*.js", script);
+
+  // 静态资源、字体和样式不需要执行编译任务，减少构建的任务提高构建效率
+  watch(
+    ["./src/assets/images/**", "./src/assets/fonts/**", "./public/**"],
+    bs.reload
+  );
+
+  return bs.init({
+    port: 2080, // 默认端口
+    open: false, // 是否自动打开
+    notify: false, // 服务器连接状态提示
+    server: {
+      baseDir: ["dist", "src", "public"],
+      routes: {
+        "/node_modules": "node_modules",
+      },
+    },
+  });
+};
+
 // 需要对外提供的任务
 const complie = parallel(page, style, script, image, font);
 const build = series(clean, parallel(complie, extra));
@@ -71,4 +152,5 @@ module.exports = {
   clean,
   complie,
   build,
+  serve,
 };
